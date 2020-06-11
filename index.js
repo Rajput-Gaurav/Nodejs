9db7734d365d65669bc051429d97c3b041aac171
rjabhi123 <64450435+rjabhi123@users.noreply.github.com>
2020-06-11 12:10:20 +0530

    Simple Node Examples

---
+++ index.js
@@ -0,0 +1,25 @@
+// examples:
+// create a object and pass two parameter onit:
+var rect = {
+            perimeter: (x,y) => (2*(x+y)),
+            area: (x,y) => (x*y)
+};
+
+// create a new function too print the values:
+function solveRect(l,b){
+    console.log("Solving the rectangle with l =" +l + "and b =" +b);
+
+    if(l <= 0 || b<= 0)
+    {
+        console.log("Rectangle dimension should be greater than zero: l =" +l + "and b=" +b);
+    }
+    else{
+        console.log("The area of the ractangle is: " +rect.area(l,b));
+        console.log("The perimeter of the rectangle is: "+rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,5);
+solveRect(0,1);
+solveRect(-3,3);
+solveRect(4,-1);
