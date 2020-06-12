09a97bc59af15b0f37a71e38154716300ba78476
rjabhi123 <64450435+rjabhi123@users.noreply.github.com>
2020-06-12 12:15:30 +0530

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// examples:
-// create a object and pass two parameter onit:
-// var rect = {
-//             perimeter: (x,y) => (2*(x+y)),
-//             area: (x,y) => (x*y)
-// };
 
 // import node_modules which we are created:
 // import rectangle node_modules:
@@ -14,14 +8,18 @@ var rect = require('./rectangle');
 function solveRect(l,b){
     console.log("Solving the rectangle with l =" +l + "and b =" +b);
 
-    if(l <= 0 || b<= 0)
-    {
-        console.log("Rectangle dimension should be greater than zero: l =" +l + "and b=" +b);
-    }
-    else{
-        console.log("The area of the ractangle is: " +rect.area(l,b));
-        console.log("The perimeter of the rectangle is: "+rect.perimeter(l,b));
-    }
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
 }
 
 solveRect(2,5);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,18 @@
 // create a node_modules:
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) =>{
+
+    if(x <=0 || y <=0)
+    // use setTimeout method and arrow function:
+    setTimeout(()=>
+                callback(new Error("Rectangle dimension should be greator than zero: l = "+ x + ", and b = " +y),
+                null),
+                2000);
+    else
+    // use setTimeout method and arrow function:
+        setTimeout(() =>
+        callback(null,{
+            perimeter: () => (2*(x+y)),
+            area:() => (x+y)
+        }),
+        2000);                
+}
